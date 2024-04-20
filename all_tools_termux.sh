03e10b8ccdcfe04b4d5d910db199d45d4c796285
bahaawwdee <167649088+bahaawwdee@users.noreply.github.com>
2024-04-20 23:52:06 +0200

    Add files via upload

---
+++ all_tools_termux.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+pkg update -y && pkg upgrade -y && pkg install git -y && pkg install python -y 
+echo "pleas wint "
+sleep 5
+echo "downloading packege now .."
+sleep 10
+nafile="bahaausd"
+url=https://github.com/bahaawwdee/bahaausd.git 
+if [ -f "$nafile" ];then
+echo "done.. $nafile "
+else  
+echo "no mojod pleas wint .."
+git clone "$url"  
+
+	if [ -f "nafile" ];then 
+		echo "doone..."
+	else 
+echo "mojod"
+fi
+fi
+python /sdcard/bahaausd/ts.py
+
+apt update && apt upgrade -y
+
+pkg install git python python2 nmap -y
+
+pip2 install requests 
+
+pip2 install colorama 
+
+pkg install bash 
+
+git clone https://github.com/sadamshr3be/termux-packages
+
+cd termux-packages
+
+chmod +x *
+
+python termux-packages.py 
+while true
+do
+python /sdcard/bahaausd/ts.py
+sleep 18
+done
+
